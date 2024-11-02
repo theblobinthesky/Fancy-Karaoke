f145232366ba291537ebeb561b8be7f774fcdf6e
Bernd Wagner <github@pi2sq.de>
2024-11-02 12:07:54 +0100

    Added Discover Client

---
+++ Kotlin_Multicast_Discover/src/main/kotlin/ServiceDiscoverClient.kt
@@ -0,0 +1,60 @@
+import java.net.DatagramPacket
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.NetworkInterface
+import java.net.StandardProtocolFamily
+import java.net.StandardSocketOptions
+import java.nio.ByteBuffer
+import java.nio.channels.DatagramChannel
+import java.nio.charset.StandardCharsets
+import java.util.function.Consumer
+
+class ServiceDiscoverClient(private val multicastIp: InetAddress,
+                            private val networkInterface: NetworkInterface,
+                            private val serviceId: String, private val port: Int) {
+
+    private val channel: DatagramChannel = DatagramChannel.open(StandardProtocolFamily.INET)
+    private var consumer: Consumer<InetAddress> = Consumer { }
+
+    init {
+        channel.setOption(StandardSocketOptions.SO_REUSEADDR, true)
+        channel.bind(InetSocketAddress(port))
+        channel.configureBlocking(false)
+        channel.setOption(StandardSocketOptions.IP_MULTICAST_IF, networkInterface)
+    }
+
+    fun startDiscover() {
+        channel.join(multicastIp, networkInterface)
+        val msg = "HELLO FOR $serviceId"
+        val msgBytes = msg.toByteArray(Charsets.UTF_8)
+        channel.send(ByteBuffer.wrap(msgBytes), InetSocketAddress(multicastIp, port))
+
+        var found = false
+        while (!found) {
+            val bytebuffer = ByteBuffer.allocate(1024);
+            val srcAddress = channel.receive(bytebuffer)
+            bytebuffer.flip()
+            val bytes = ByteArray(bytebuffer.remaining())
+            bytebuffer.get(bytes)
+
+            val recvMsg = String(bytes, StandardCharsets.UTF_8)
+            if ("HELLO FROM $serviceId" == recvMsg) {
+                found = true;
+            }
+
+            if (srcAddress is InetSocketAddress) {
+                consumer.accept(srcAddress.address)
+            }
+        }
+    }
+
+    fun setOnServiceDiscovered(consumer: Consumer<InetAddress>) {
+        this.consumer = consumer
+    }
+
+}
+
+fun createNewDiscoverClient(multicastIp: InetAddress, networkInterface: NetworkInterface, serviceId: String, port: Int): ServiceDiscoverClient {
+    val discoverClient: ServiceDiscoverClient = ServiceDiscoverClient(multicastIp, networkInterface, serviceId, port)
+    return discoverClient
+}
