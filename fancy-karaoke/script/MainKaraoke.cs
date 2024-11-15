4d8d8608e1c0331120b6bab7db419a3e05c9767b
Bernd Wagner <github@pi2sq.de>
2024-11-15 21:17:06 +0100

    Added songs lyrics

--- fancy-karaoke/script/MainKaraoke.cs
+++ fancy-karaoke/script/MainKaraoke.cs
@@ -11,6 +11,10 @@ public partial class MainKaraoke : Node2D
 	private int own_note_count = 0;
 	
 	private int last_mod_pos = 0;
+	
+	private Godot.Collections.Array lyrics = new Godot.Collections.Array();
+	private int line_index = -1;
+	private int word_index = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -31,6 +35,46 @@ public partial class MainKaraoke : Node2D
 		var pos = cam.Position;
 		pos.X = speed * (float) time;
 		cam.Position = pos;
+		
+		HBoxContainer lyricsContainer = GetNode<HBoxContainer>("KaraokeCam/Control/LyricsContainer");
+		
+		if (line_index < lyrics.Count) {
+			if (line_index >= 0) {
+				Godot.Collections.Dictionary line = lyrics[line_index].As<Godot.Collections.Dictionary>();
+				Godot.Collections.Array words = line["word_finished"].As<Godot.Collections.Array>();
+			
+				if (line["offset"].As<float>() + words[words.Count - 1].As<float>() + 0.5f < time) {
+					foreach (Node child in lyricsContainer.GetChildren())
+					{
+						lyricsContainer.RemoveChild(child);
+						child.QueueFree();
+					}
+				} else if (word_index < words.Count && line["offset"].As<float>() + words[word_index].As<float>() < time) {
+					((Label) lyricsContainer.GetChildren()[word_index]).LabelSettings = GD.Load<LabelSettings>("res://fonts/lyrics_past.tres");
+					word_index++;
+				}
+			}
+			
+			if (line_index < lyrics.Count - 1 && lyrics[line_index + 1].As<Godot.Collections.Dictionary>()["offset"].As<float>() - 0.5f < time) {
+				line_index++;
+				word_index = 0;
+				
+				foreach (Node child in lyricsContainer.GetChildren())
+				{
+					lyricsContainer.RemoveChild(child);
+					child.QueueFree();
+				}
+				
+				Godot.Collections.Dictionary nextLine = lyrics[line_index].As<Godot.Collections.Dictionary>();
+				string[] wordsText = nextLine["line"].As<string>().Split(" ");
+				foreach (string wordText in wordsText) {
+					Label word = new Label();
+					word.LabelSettings = GD.Load<LabelSettings>("res://fonts/lyrics_future.tres");
+					word.Text = wordText;
+					lyricsContainer.AddChild(word);
+				}
+			}
+		}
 		
 		var mod_pos = ((int) Math.Floor(pos.X)) % 2000;
 		
@@ -59,7 +103,8 @@ public partial class MainKaraoke : Node2D
 		
 		GDScript DataLoad = GD.Load<GDScript>("res://script/DataLoader.gd");
 		GodotObject dataLoad = (GodotObject) DataLoad.New();
-		Godot.Collections.Array notes = (Godot.Collections.Array) dataLoad.Call("read_note_data", name + "_notes");
+		Godot.Collections.Array notes = (Godot.Collections.Array) dataLoad.Call("read_note_data", name);
+		lyrics = (Godot.Collections.Array) dataLoad.Call("read_lyrics_data", name);
 		
 		NotenLeiste main = GetNode<NotenLeiste>("NotenLeiste");
 		main.load_notes(notes);
